66cea05156e4db0639e5349ac45bc154faa0e262
jpd <john.dandison@live.com>
2017-08-10 15:10:40 -0400

    Add project files.

---
+++ ASB.PubPolicy.SASGen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+
+namespace ASB.PubPolicy.SASGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class Generator
+    {
+        private const string Namespace = "sb://jpd-pp.servicebus.windows.net";
+        private const string EventHubName = "hub";
+        private const string PrimaryKey = "XnbXTS0FRCu4q8hGbvRvCuOUeJCq3f3wbRcKNKs5ofk=";
+
+        public static string GetSas(string publisherId)
+        {
+            return SharedAccessSignatureTokenProvider.GetPublisherSharedAccessSignature(new Uri(Namespace), EventHubName, publisherId, "Send", PrimaryKey, new TimeSpan(0, 5, 0));
+        }
+    }
+}
---
+++ ASB.PubPolicy/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ASB.PubPolicy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eps = "<SAS gen endpoint>";
+
+            var sb = "<service bus endpoint>";
+            var hub = "hub";
+            var iterations = 1000000;
+
+            Console.WriteLine("Running...");
+            try
+            {
+                Parallel.For(0, iterations, i =>
+                {
+                    var sas = new SasPublisherClient(eps, $"sas-{i}-{DateTime.UtcNow.Ticks}", sb, hub);
+                    //Console.WriteLine($"=== Creating publisher {i} of {iterations} ===");
+                    sas.Send("hey");
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("All finished.");
+            Console.ReadLine();
+        }
+    }
+
+    public class SasPublisherClient
+    {
+        private readonly string _endpointService;
+        private readonly string _publisherName;
+        private readonly string _sbNamespace;
+        private readonly string _hubName;
+
+        public SasPublisherClient(string endpointService, string publisherName, string sbNamespace, string hubName)
+        {
+            _endpointService = endpointService;
+            _publisherName = publisherName;
+            _sbNamespace = sbNamespace;
+            _hubName = hubName;
+        }
+
+        private string GetSas()
+        {
+            //Console.WriteLine($"Getting SAS for {_publisherName}...");
+            var url = $"{_endpointService}&publisherId={_publisherName}";
+            var nc = new WebClient();
+            return nc.DownloadString(url).Trim('"');
+        }
+
+        public void Send(string a)
+        {
+            var sas = GetSas();
+            //Console.WriteLine("Creating EHSender");
+            var xon = ServiceBusConnectionStringBuilder.CreateUsingSharedAccessSignature(new Uri(_sbNamespace), _hubName, _publisherName, sas);
+            var c = EventHubSender.CreateFromConnectionString(xon);
+            c.SendAsync(new EventData(System.Text.Encoding.UTF8.GetBytes($"{_publisherName} says {a}")));
+            //Console.WriteLine("Sent.");
+        }
+    }
+}
